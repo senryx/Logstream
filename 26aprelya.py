f089ef8c470113e7b71a210dbdec8bd648d6d070
senryx <max001554@mail.ru>
2022-04-26 20:22:58 +0400

    Исправление задачи на високосный год

--- 26aprelya.py
+++ 26aprelya.py
@@ -17,7 +17,7 @@ def sorddict(a):
     return sorted(a, key=a.get, reverse=True)[:3] 
 
 def is_year_leap(a):
-    if a%4==0 or a%400==0:
+    if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
         return True
     else:
         return False
@@ -26,4 +26,4 @@ if __name__ == "__main__":
     #print(arr1())
     #print(sorddict({'a': 500, 'b': 5874, 'c': 560,'d': 400, 'e': 5874, 'f': 20}))
     print(is_year_leap(1764))
-    print(is_year_leap(2017))
+    print(is_year_leap(1700))
